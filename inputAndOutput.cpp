47f9e990446a0ecdb366186c0eb8984092f9d463
rakibunateed <rakibunateed@gmail.com>
2024-07-05 21:02:26 +0600

    3

---
+++ inputAndOutput.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    
+    cout<<"Enter the value of number one: ";
+    cin>>num1;
+
+    
+    cout<<"Enter the value of number two: ";
+    cin>>num2;
+
+    int sum = num1 + num2;
+    cout<<"The some of two numbers: "<<sum;
+    
+    
+    return 0;
+}
