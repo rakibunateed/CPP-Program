4726ee9913f1714ce6cd05046b26c077c5f95ec6
rakibunateed <rakibunateed@gmail.com>
2024-07-10 23:19:51 +0600

    functions

---
+++ functions.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum(int a,int b){
+    int c = a+b;
+    return c;
+   
+}
+
+int main(){
+    int num1,num2;
+    cin>>num1>>num2;
+    cout<<sum(num1,num2);
+  
+    
+    
+    return 0;
+}
